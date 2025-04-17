af2289bdca0f56d956a4c5c55b9b985b4f163bfe
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-18 01:17:04 +0530

    Done: express basics

--- 02-express-tutorial/final/03-express-basics.js
+++ 02-express-tutorial/final/03-express-basics.js
@@ -1,27 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-  console.log('user hit the resource')
-  res.status(200).send('Home Page')
+    console.log('user hits the resource');
+    res.status(200).send('Home page');
 })
 
 app.get('/about', (req, res) => {
-  res.status(200).send('About Page')
+    console.log('user hits the resource');
+    res.status(200).send('About page');
 })
 
 app.all('*', (req, res) => {
-  res.status(404).send('<h1>resource not found</h1>')
+    res.status(404).send('<h1>Resource not found!!!</h1>');
 })
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000...')
+    console.log('server is listening on port 5000...');
 })
+
+// get, post, put, delete: http verbs
 
 // app.get
 // app.post
 // app.put
 // app.delete
 // app.all
-// app.use
+// app.use: Responsible for middleware
 // app.listen
