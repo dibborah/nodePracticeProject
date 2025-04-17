e268cf1e32ebcb24e18bb82d649e491f76069192
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-18 02:55:12 +0530

    Completed: Basic json()

--- 02-express-tutorial/final/06-basic-json.js
+++ 02-express-tutorial/final/06-basic-json.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const app = express()
-const { products } = require('./data')
+// API vs SSR
+
+// with express 
+// We will use mostly one of the two options
+
+// 1. setup API: res.json() {does most heavylifting works}, api-json, send data
+// 2. templates with SSR(whole html, css, js template from server): res.render, ssr template, res template
+
+const express = require('express');
+const { products } = require('./data');
+const app = express();
+
+// harding coded response data
+// app.get('/', (req, res) => {
+//     res.json([{ name: 'susan' }, { name: 'ravi' }]);
+// })
+
 app.get('/', (req, res) => {
-  res.json(products)
+    res.json(products);
 })
 
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
+app.listen(5000, (req, res) => {
+    console.log('server is listening in port 5000......');
 })
