3dd02f609df3657135e63bd1d716a575e591cff6
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-21 16:07:02 +0530

    Done: query string

---
+++ 02-express-tutorial/final/prac8.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const { products } = require('./data');
+const app = express();
+
+// app.get('/', (req, res) => {
+//     // res.json([{ name: 'susan' }, { name : 'john' }]);
+//     res.json(products);
+// })
+
+// default 404 response from express when the resource is not found
+
+app.get('/', (req, res) => {
+    res.send('<h1>Home page</h1><a href="/api/products">products</a>');
+})
+
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const {id, image, name} = product;
+        return {id, image, name};
+    })
+    res.send(newProducts);
+    // res.send(products);
+})
+
+// using route parameters or route params or placeholders
+app.get('/api/products/:productID', (req, res) => {
+    console.log('productID', req.params.productID);
+    const singleProduct = products.find((product) => (
+        product.id === +req.params.productID
+    ))
+    if(!singleProduct) {
+        res.send('Product does not exist!!!');
+    }
+    res.send(singleProduct);
+})
+
+// : are route parameter or placehoders
+app.get('/api/products/:productID/reviews/:reviewsID', (req, res) => {
+    console.log(req.params);
+    res.send("Hello world!!!");
+})
+
+app.get('/api/v1/query', (req, res) => {
+    const { search, limit } = req.query;
+    let sortedProducts = [...products];
+    if(search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search);
+        })
+    }
+    if(limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit));
+    }
+    if(sortedProducts?.length < 1) {
+        // res.status(200).send("<h1>No matched products found!!!</h1>");
+        return res.status(200).json({ success: true, data: [] });
+        // res.status(200).json({ success: true, data: [] });
+    }
+    res.status(200).json(sortedProducts);
+    // res.send('hello world!!!');
+})
+
+app.listen( 5000, (req, res) => {
+    console.log('server is listening in port 5000...');
+})
+
+// Query Strings: It is a way to send small amounts of information to the server using the urls 
